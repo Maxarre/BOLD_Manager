0937d96b4659280864867c1b44ad67aa1e2c8b11
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-20 18:48:23 +0200

    tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,38 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+    @task = Task.new(params[:task])
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to
   end
 
   def edit
+    @task = Task.find(params[:id])
+    redirect_to
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    redirect_to
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @list.destroy
+    redirect_to
   end
 
   private
