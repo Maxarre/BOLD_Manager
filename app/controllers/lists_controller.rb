3c2550e024c49628e4cff9717dfd536fc971c1fa
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-20 18:42:23 +0200

    lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,23 +1,38 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
+    @list = List.new(params[:list])
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to
   end
 
   def edit
+    @list = List.find(params[:id])
+    redirect_to
   end
 
   def update
+    @list = List.find(params[:id])
+    @list.update(params[:list])
+    redirect_to
   end
 
   def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to
   end
 
   private
