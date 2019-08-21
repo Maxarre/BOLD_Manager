0b1ce53eed6a53d329cf93a34858b2b14760e730
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-21 21:55:02 +0200

    sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
   private
 
   def layout_by_resource
