474fd9a072043dd5e0ddb6b2e0c29fe52d65d66f
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-21 07:00:03 +0200

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,33 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new(:user)
   end
 
   def create
+    @user = User.find(params[:user])
+    @user.save
+    redirect_to
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+    redirect_to user_path(@user)
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
+  # get 'tasks/index'
+  # get 'tasks/show'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
+  # get 'lists/edit'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
   devise_for :users
   root to: 'pages#home'
+  resources :users
+  resources :lists
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
