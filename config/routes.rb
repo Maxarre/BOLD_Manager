61f87834cf474e5ee39d8d8320cddda7f4508460
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-21 16:30:52 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   # get 'users/edit'
   devise_for :users
   root to: 'users#dashboard'
+  get 'home', to: 'pages#home'
+  get 'dashboard', to: 'users#dashboard'
   resources :users
   resources :lists
   resources :tasks
