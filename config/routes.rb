3932b2f077b1794eb3937b05b7af1ade24cfe3fb
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-26 02:46:56 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
-  # get 'tasks/index'
-  # get 'tasks/show'
-  # get 'tasks/new'
-  # get 'tasks/edit'
-  # get 'lists/index'
-  # get 'lists/show'
-  # get 'lists/new'
-  # get 'lists/edit'
-  # get 'users/index'
-  # get 'users/show'
-  # get 'users/new'
-  # get 'users/edit'
   devise_for :users
+
   root to: 'users#dashboard'
+
   get 'home', to: 'pages#home'
   get 'dashboard', to: 'users#dashboard'
+
   resources :users
-  resources :lists
-  resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :tasks, only: [:create, :destroy]
+  end
 end
